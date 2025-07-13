f5e6d98aa0065691efe65cbfccd905ce9f82bf3e
FTTroy <troianofrancesco01@gmail.com>
2025-07-13 20:52:58 +0200

    ping controller

---
+++ src/main/java/com/github/fttroy/love_sender/controller/PingController.java
@@ -0,0 +1,20 @@
+package com.github.fttroy.love_sender.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Slf4j
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+
+    @GetMapping("/")
+    public void ping() {
+        log.info("pinged at {}", ZonedDateTime.now(ZoneId.of("Europe/Rome")).toLocalDateTime());
+    }
+}
